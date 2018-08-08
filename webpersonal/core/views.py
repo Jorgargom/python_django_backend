a329af5d36b70adc693ec9ad310f99b29132e77e
Jorge García Gómez <jorge@jorgegarciagomez.es>
2018-08-08 15:50:04 +0200

    comienzo con los template

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render, HttpResponse
 
+
+html_base ="""
+<h1>Mi web personal</h1>
+<ul>
+
+<li><a href="/">Portada</a></li>
+<li><a href="/about-me/">Acerca de</a></li>
+<li><a href="/portfolio/">Portfolio</a></li>
+<li><a href="/contacto/">Contacto</a></li>
+
+</ul>
+"""
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Hola que ase</h1>")
+    return HttpResponse(html_base + """
+    <h2>Hola que ase</h2>
+    """)
 
+def about(request):
+    return HttpResponse(html_base + """
+    <h2>asiendo cosas</h2>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+    <h2>muchas cosas</h2>
+    """)
+
+def contacto(request):
+    return HttpResponse(html_base + """
+    <h2>contratame</h2>
+    """)
