8e8ee735d2205d7e59f84a2c50613d08454869ee
Jorge García Gómez <jorge@jorgegarciagomez.es>
2018-08-08 15:59:14 +0200

    paginas configuradas con render-request

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,34 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
 
-html_base ="""
-<h1>Mi web personal</h1>
-<ul>
-
-<li><a href="/">Portada</a></li>
-<li><a href="/about-me/">Acerca de</a></li>
-<li><a href="/portfolio/">Portfolio</a></li>
-<li><a href="/contacto/">Contacto</a></li>
-
-</ul>
-"""
-
 # Create your views here.
 
 def home(request):
     return render(request, "core/home.html")
 
-def about(request):
-    return HttpResponse(html_base + """
-    <h2>asiendo cosas</h2>
-    """)
+def contacto(request):
+    return render(request, "core/contacto.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-    <h2>muchas cosas</h2>
-    """)
+    return render(request, "core/portfolio.html")
 
-def contacto(request):
-    return HttpResponse(html_base + """
-    <h2>contratame</h2>
-    """)
+def about(request):
+    return render(request, "core/about.html")
