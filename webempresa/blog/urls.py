b8fc719c0e566b6a886a5f9822046fa0cf03e385
Jorge García Gómez <jorge@jorgegarciagomez.es>
2018-08-11 09:31:50 +0200

    continuo solucionando cosillas

--- webempresa/blog/urls.py
+++ webempresa/blog/urls.py
@@ -13,29 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
-from django.conf import settings
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
 
 
-    #Path del core
-    path ('', include('core.urls')),
+    path('blog/', views.blog, name="blog"),
 
 
-    # Paths de blog
-    path ('blog/', include('blog.urls')),
-
-    # Paths de services
-    path ('services/', include('services.urls')),
-
-    #Path del admin
-    path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
