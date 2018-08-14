b473fb59f28e65f627b9603fcd0d7431ecba7f27
Jorge García Gómez <jorge@jorgegarciagomez.es>
2018-08-14 17:23:21 +0200

    añado app para las paginas de privacidad

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -8,16 +8,10 @@ def home(request):
 def about(request):
     return render(request, "core/about.html")
 
-
-
 def store(request):
     return render(request, "core/store.html")
 
 def contact(request):
     return render(request, "core/contact.html")
 
-
-
-def sample(request):
-    return render(request, "core/sample.html")
 
--- webempresa/pages/urls.py
+++ webempresa/pages/urls.py
@@ -13,30 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
-
-    # Path del core
-    path ('', include('core.urls')),
-
-    # Paths de services
-    path ('services/', include('services.urls')),
-
-    # Paths de blog
-    path ('blog/', include('blog.urls')),
-
-    # Paths de pages
-    path ('page/', include('pages.urls')),
-
-    # Path del admin
-    path('admin/', admin.site.urls),
+    path('<int:page_id>/', views.page, name="page"),
 ]
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
