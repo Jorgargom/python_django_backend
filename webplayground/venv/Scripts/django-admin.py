d2b764ddd27e2e3fbacde1f6afbbf57a2f99b740
Jorge García Gómez <jorge@jorgegarciagomez.es>
2018-10-10 09:51:09 +0200

    creando app corre y pages

---
+++ webplayground/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jorge\Documents\GitHub\python_django_backend\webplayground\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
